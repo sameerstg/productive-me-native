4928f15c02c219fb315c23e75470082ef44ae34c
unknown <sameerstg@outlook.com>
2023-12-24 14:24:28 +0500

    tailswind

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import {  Text, View } from 'react-native';
 
 export default function TabOneScreen() {
   return (
-    <View>
-      <Text >asfkfjasfkasfjaslfjkasjflj</Text>
+    <View className='bg-red-300'>
+      <Text >sasfasf</Text>
     </View>
   );
 }
